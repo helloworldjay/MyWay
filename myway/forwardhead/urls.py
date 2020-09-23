8ad256d4b815af28cb0522607a53057b877cbbb8
unknown <skekdnl12@gmail.com>
2020-09-24 00:26:52 +0900

    2020.09.24
    views, urls변경

--- myway/forwardhead/urls.py
+++ myway/forwardhead/urls.py
@@ -1,5 +1,7 @@
 from django.urls import include, path
 
+from . import views
 urlpatterns = [
-
+    path('index/', views.index, name='index'),
+    path('contact/', views.contact, name='contact')
 ]
