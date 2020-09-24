01706900699abc542e34218f797a32e0a8c6a85c
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2020-09-24 16:08:53 +0900

    Home, FHP_explain

--- myway/myway/settings.py
+++ myway/myway/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'forwardhead',
     'accounts',
+    'base',
+
 ]
 
 MIDDLEWARE = [
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
