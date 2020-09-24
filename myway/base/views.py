2f0acf3b03f9bb199321e95b4abbe8a39b8d3168
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2020-09-24 16:45:39 +0900

    build home

--- myway/base/views.py
+++ myway/base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def get_home(request):
-    return render(request, 'base.html', {})
+    return render(request, 'base/index.html', {})
 
 def get_contact(request):
-    return render(request, 'contact.html', {})
+    return render(request, 'base/contact.html', {})
